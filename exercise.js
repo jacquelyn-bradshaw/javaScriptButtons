0aef588eec96ad4a00cda99f5f6859cab3067b01
Jacquelyn Bradshaw <jacquelyn_bradshaw@live.co.uk>
2022-12-31 19:55:40 +0000

    stored elements in variables

--- exercise.js
+++ exercise.js
@@ -2,7 +2,9 @@
 
 // 1) Select the two <button> elements and store them in two different variables.
 //    - Select the first button without adding or using any "id"
+let removeParagraphButton = document.querySelector("button");
 //    - Select the second button by using an "id"
+let addColourButton = document.getElementById("paragraph-colour-button");
 // 2) Add "click" event listener to both buttons (with two different functions).
 //    The functions should "console.dir()" the clicked buttons.
 //    - Output the first button by using the variable in which it's stored
